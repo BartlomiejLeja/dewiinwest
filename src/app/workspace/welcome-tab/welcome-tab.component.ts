49058ac36d791cc7721b5ae1fc1f7e34afd314d6
Bartlomiej Leja <bleja@prescientsaas.com>
2020-02-03 08:46:36 +0100

    addded activate link on drop box and added margin on apartment properties

--- src/app/workspace/welcome-tab/welcome-tab.component.ts
+++ src/app/workspace/welcome-tab/welcome-tab.component.ts
@@ -22,80 +22,6 @@ export class WelcomeTabComponent implements OnInit, OnDestroy {
   public backgroundCounterUrl ='assets/test.jpg'
   private _mobileQueryListener: () => void;
   public carouselItems: any;
-
-  constructor( 
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
-    private changeDetectorRef: ChangeDetectorRef, 
-    private media: MediaMatcher, 
-    private router: Router,
-    private selectedRouteService: SelectedRouteService
- ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-
-    this.matIconRegistry.addSvgIcon(
-      "arrow",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/arrow.svg")
-    );
-
-    this.matIconRegistry.addSvgIcon(
-      "left-arrow",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/arrow_back_ios-24px.svg")
-    );
-  }
- 
-  @Input() timing = '350ms ease-in';
-  @ViewChild('test',{static: false}) private carousel : ElementRef;
-
-  @ViewChild('counter',{static: false}) chatTeaser: ElementRef;
-  @ViewChild('counter1',{static: false}) chatTeaser1: AnimatedCounterComponent;
-  @ViewChild('counter2',{static: false}) chatTeaser2: AnimatedCounterComponent;
-  @ViewChild('counter3',{static: false}) chatTeaser3: AnimatedCounterComponent;
-
-  @HostListener('window:scroll')
-  checkScroll() {
-    const scrollPosition = window.pageYOffset + window.innerHeight;
-    
-    if (this.chatTeaser && this.chatTeaser.nativeElement.offsetTop >= scrollPosition) {
-        this.chatTeaser1.animateCount();
-        this.chatTeaser2.animateCount();
-        this.chatTeaser3.animateCount();
-    }
-}
-
-  ngOnInit() {
-    this.selectedRouteService.routerChanged('main');
-    this.realEstateFlipImages = [
-      { 
-        realEstateDescription: 'Nowoczesny dom wielorodziny w nowej hucie', 
-        realEstateImageUrl: 'assets/apartamenty-nad-wilga-opatkowice/orginal2-1200x750.jpg',
-        realEstateName: 'Apartamenty nad Wilgą'
-      },
-      { 
-        realEstateDescription: 'Słoneczne osiedle na Wadowskiej',
-        realEstateImageUrl: 'https://i.imgur.com/nJsfIPz.jpg',
-        realEstateName: 'Słoneczna Wadowska'
-      },
-      { realEstateDescription: 'Kameralne domki jednorodzine',
-        realEstateImageUrl: 'assets/LA-DUBOIS/orginal2-1200x750.png',
-        realEstateName: 'LA DUBOIS'
-      }
-    ]
-
-    this.carouselItems = [
-      { imageUrl: 'https://i.imgur.com/gZpglkA.png', title : 'Budujemy twoje marzenia', description: 'Twój Dom Group', isWelcomeImage: true },
-      { imageUrl: 'https://i.imgur.com/nJsfIPz.jpg', title : 'Sloneczna Wadowska', description: 'Blisko do Krakowa, blisko do natury', isWelcomeImage: false  },
-      { imageUrl: 'https://i.imgur.com/3j0NYXy.png',title : 'LA-DUBOIS', description: 'Nowoczesne osiedle, w pięknym otoczeniu', isWelcomeImage: false  },
-    ]
-  }
-
-  public goTocurrentInvestments(){
-    this.selectedRouteService.routerChanged('aktualnaoferta');
-    this.router.navigate(['/inwestycjeaktualne']);
-  }
-
   public svgIconOverrides: SvgIconOverrides = {
     arrowBack: 'left-arrow',
     arrowForward: 'arrow'
@@ -121,6 +47,78 @@ export class WelcomeTabComponent implements OnInit, OnDestroy {
   public orientation: Orientation = 'ltr';
   public log: string[] = [];
 
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher,
+    private router: Router,
+    private selectedRouteService: SelectedRouteService
+ ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+
+    this.carouselItems = [
+      { imageUrl: 'https://i.imgur.com/gZpglkA.png', title : 'Budujemy twoje marzenia', description: 'Twój Dom Group', isWelcomeImage: true },
+      { imageUrl: 'https://i.imgur.com/nJsfIPz.jpg', title : 'Sloneczna Wadowska', description: 'Blisko do Krakowa, blisko do natury', isWelcomeImage: false  },
+      { imageUrl: 'https://i.imgur.com/3j0NYXy.png',title : 'LA-DUBOIS', description: 'Nowoczesne osiedle, w pięknym otoczeniu', isWelcomeImage: false  },
+    ];
+
+    this.matIconRegistry.addSvgIcon(
+      "arrow",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/arrow.svg")
+    );
+
+    this.matIconRegistry.addSvgIcon(
+      "left-arrow",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/arrow_back_ios-24px.svg")
+    );
+  }
+ 
+  @Input() timing = '350ms ease-in';
+  @ViewChild('test',{static: false}) private carousel : ElementRef;
+
+  @ViewChild('counter',{static: false}) chatTeaser: ElementRef;
+  @ViewChild('counter1',{static: false}) chatTeaser1: AnimatedCounterComponent;
+  @ViewChild('counter2',{static: false}) chatTeaser2: AnimatedCounterComponent;
+  @ViewChild('counter3',{static: false}) chatTeaser3: AnimatedCounterComponent;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = window.pageYOffset + window.innerHeight;
+    if (this.chatTeaser && this.chatTeaser.nativeElement.offsetTop >= scrollPosition) {
+        this.chatTeaser1.animateCount();
+        this.chatTeaser2.animateCount();
+        this.chatTeaser3.animateCount();
+    }
+}
+
+  ngOnInit() {
+    this.selectedRouteService.routerChanged('main');
+    this.realEstateFlipImages = [
+      { 
+        realEstateDescription: 'Nowoczesny dom wielorodziny w nowej hucie', 
+        realEstateImageUrl: 'assets/apartamenty-nad-wilga-opatkowice/orginal2-1200x750.jpg',
+        realEstateName: 'Apartamenty nad Wilgą'
+      },
+      { 
+        realEstateDescription: 'Słoneczne osiedle na Wadowskiej',
+        realEstateImageUrl: 'https://i.imgur.com/nJsfIPz.jpg',
+        realEstateName: 'Słoneczna Wadowska'
+      },
+      { realEstateDescription: 'Kameralne domki jednorodzine',
+        realEstateImageUrl: 'assets/LA-DUBOIS/orginal2-1200x750.png',
+        realEstateName: 'LA DUBOIS'
+      }
+    ]
+  }
+
+  public goTocurrentInvestments(){
+    this.selectedRouteService.routerChanged('aktualnaoferta');
+    this.router.navigate(['/inwestycjeaktualne']);
+  }
+
   onChange($event){
     //this.prev1()
   }
@@ -128,7 +126,6 @@ export class WelcomeTabComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-  
 }
 
 interface RealEstateFlipImage {
